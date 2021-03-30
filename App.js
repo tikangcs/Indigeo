9641057af1c7270ceec6e39fee727db9eda19512
Tim Kang <tikang.cs@gmail.com>
2021-03-30 16:32:41 -0400

    #4 - Deployed Google Maps to app

--- App.js
+++ App.js
@@ -1,37 +1,40 @@
 import * as React from 'react';
 import { StyleSheet, Text, View, Dimensions, SafeAreaView, Button} from 'react-native';
 import {useDimensions, useDeviceOrientation} from '@react-native-community/hooks';
-import MapView from 'react-native-maps';
+import {MapView, PROVIDER_GOOGLE } from 'react-native-maps';
 import { StatusBar } from 'expo-status-bar';
-import MapScreen from './app/screens/MapScreen.js'
-import ViewItemScreen from './app/screens/ViewItemScreen.js'
+import MapScreen from './app/screens/MapScreen.js';
+import ViewItemScreen from './app/screens/ViewItemScreen.js';
 
 export default function App() {
   // console.log(useDimensions())
   // console.log(useDeviceOrientation())
 
   return (
-    <ViewItemScreen />
-    // <SafeAreaView style={styles.container}>
-    //   <StatusBar style="auto" />
-    //   <MapScreen />
-    //   <View style={styles.filter}>
-    //     <Button
-    //       style={styles.filterButtons}
-    //       title="Flora"
-    //       onPress={()=> Alert.alert('Filtered for Flora')}
-    //       accessibilityLabel='Filter for Flora'
-    //       color="orange"
-    //     />
-    //     <Button
-    //       style={styles.filterButtons}
-    //       title="Fauna"
-    //       onPress={()=> Alert.alert('Filtered for Fauna')}
-    //       accessibilityLabel='Filter for Fauna'
-    //       color="orange"
-    //     />
-    // </View>
-    // </SafeAreaView>
+    //MAP SCREEN
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="auto" />
+      <MapScreen provider={PROVIDER_GOOGLE} />
+      <View style={styles.filter}>
+        <Button
+          style={styles.filterButtons}
+          title="Flora"
+          onPress={()=> Alert.alert('Filtered for Flora')}
+          accessibilityLabel='Filter for Flora'
+          color="orange"
+        />
+        <Button
+          style={styles.filterButtons}
+          title="Fauna"
+          onPress={()=> Alert.alert('Filtered for Fauna')}
+          accessibilityLabel='Filter for Fauna'
+          color="orange"
+        />
+    </View>
+    </SafeAreaView>
+
+    // //VIEW ITEMS SCREEN
+    // <ViewItemScreen />
   );
 }
 
