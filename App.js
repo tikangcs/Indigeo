7fc9bef7f4a9c301bc6ad1570756241693a5a41b
Tim Kang <tikang.cs@gmail.com>
2021-03-31 14:50:14 -0400

    #16 - Implemented the design of Home screen

--- App.js
+++ App.js
@@ -3,18 +3,18 @@ import { StyleSheet, Image, Text, View, Dimensions, SafeAreaView, Button} from '
 import {useDimensions, useDeviceOrientation} from '@react-native-community/hooks';
 import MapView from 'react-native-maps';
 import { StatusBar } from 'expo-status-bar';
-import Home from './app/screens/Home.js'
+import HomeScreen from './app/screens/HomeScreen.js'
 import FloraScreen from './app/screens/FloraScreen.js';
 import FaunaScreen from './app/screens/FaunaScreen.js';
 import ViewItemScreen from './app/screens/ViewItemScreen.js';
 
 export default function App() {
-  const [currentView, setCurrentView] = useState('Fauna');
+  const [currentView, setCurrentView] = useState('Home');
   return (
     currentView === 'Flora' ? <FloraScreen /> :
     currentView === 'Fauna' ? <FaunaScreen /> :
     currentView === 'Item' ? <ViewItemScreen />:
-    <Home />
+    <HomeScreen />
   );
 }
 
---
+++ app/screens/HomeScreen.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { ImageBackground, StyleSheet, View, Text, Image } from "react-native";
+
+export default function HomeScreen(props) {
+  return (
+    <ImageBackground
+      style={styles.background}
+      source={require('../assets/Home5.jpg')}
+    >
+      <Image
+        style={styles.logo}
+        source={require('../assets/Logo2.png')} />
+      <View style={styles.buttonContainer}>
+        <View style={styles.floraButton}>
+          <Text style={styles.buttonText}>FLORA</Text>
+        </View>
+        <View style={styles.faunaButton}>
+          <Text style={styles.buttonText}>FAUNA</Text>
+        </View>
+      </View>
+
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+  buttonContainer: {
+    flexDirection:'row'
+  },
+  floraButton: {
+    width: '50%',
+    height: 70,
+    backgroundColor: 'rgba(166, 204, 241, 0.7)',
+    flexDirection: 'row',
+    borderWidth: 1,
+  },
+  faunaButton: {
+    width: '50%',
+    height: 70,
+    backgroundColor: 'rgba(166, 204, 241, 0.7)',
+    flexDirection: 'row',
+    borderWidth: 1,
+    color: 'rgba(3, 54, 1, 1)',
+  },
+  logo: {
+    position: 'absolute',
+    top: 40,
+    alignSelf: 'center',
+    height: 300,
+    width: 300,
+    resizeMode: 'stretch'
+  },
+  buttonText: {
+    color: 'rgb(92,50,3)',
+    fontSize: 30,
+    flexDirection: 'column',
+    alignSelf: 'center',
+    left: 60
+  }
+})
