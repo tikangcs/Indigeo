905f5761fcebd7b92be820a49151bbb95d418d92
Tim Kang <tikang.cs@gmail.com>
2021-03-30 21:06:42 -0400

    #8 - Added stock images and set markers on map

--- App.js
+++ App.js
@@ -5,42 +5,40 @@ import MapView from 'react-native-maps';
 import { StatusBar } from 'expo-status-bar';
 import MapScreen from './app/screens/MapScreen.js';
 import ViewItemScreen from './app/screens/ViewItemScreen.js';
+import MarkerScreen from './app/screens/MarkerScreen.js';
 
 export default function App() {
   // console.log(useDimensions())
   // console.log(useDeviceOrientation())
 
   return (
-    //MAP SCREEN
-    <SafeAreaView style={styles.container}>
-      <StatusBar style="auto" />
-      <MapScreen
-       showsUserLocation='true'
-       initialRegion={{
-       latitude: 37.78825,
-       longitude: -122.4324,
-       latitudeDelta: 0.0922,
-       longitudeDelta: 0.0421}} />
-      <View style={styles.filter}>
-        <Button
-          style={styles.filterButtons}
-          title="Flora"
-          onPress={()=> Alert.alert('Filtered for Flora')}
-          accessibilityLabel='Filter for Flora'
-          color="orange"
-        />
-        <Button
-          style={styles.filterButtons}
-          title="Fauna"
-          onPress={()=> Alert.alert('Filtered for Fauna')}
-          accessibilityLabel='Filter for Fauna'
-          color="orange"
-        />
-    </View>
-    </SafeAreaView>
+    // //MAP SCREEN
+    // <SafeAreaView style={styles.container}>
+    //   <StatusBar style="auto" />
+    //   <MapScreen />
+    //   <View style={styles.filter}>
+    //     <Button
+    //       style={styles.filterButtons}
+    //       title="Flora"
+    //       onPress={()=> Alert.alert('Filtered for Flora')}
+    //       accessibilityLabel='Filter for Flora'
+    //       color="orange"
+    //     />
+    //     <Button
+    //       style={styles.filterButtons}
+    //       title="Fauna"
+    //       onPress={()=> Alert.alert('Filtered for Fauna')}
+    //       accessibilityLabel='Filter for Fauna'
+    //       color="orange"
+    //     />
+    // </View>
+    // </SafeAreaView>
 
     // //VIEW ITEMS SCREEN
     // <ViewItemScreen />
+
+    //MARKER VIEW SCREEN
+    <MarkerScreen />
   );
 }
 
--- app/screens/MapScreen.js
+++ app/screens/MapScreen.js
@@ -9,11 +9,11 @@ const DATA = [
   },
   {
     id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Roses',
+    title: 'Chrysanthemum',
   },
   {
     id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Orchids',
+    title: 'Lavender',
   },
   {
     id: '58694a0f-3da1-471f-bd96-145571e29d73',
@@ -21,7 +21,7 @@ const DATA = [
   },
   {
     id: '58694a0f-3da1-471f-bd96-145571e29d74',
-    title: 'Hydragias',
+    title: 'Hydrangeas',
   },
 ];
 
