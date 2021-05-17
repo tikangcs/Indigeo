92d74f0e6f1b56840f64c37f8a20d2c500368f7e
Tim Kang <tikang.cs@gmail.com>
2021-05-17 11:49:01 -0400

    #50 - Minor update to profile

--- app/utils/profile.js
+++ app/utils/profile.js
@@ -1,70 +1,8 @@
 module.exports = {
   username: "Tim Kang",
   email: "indigeo@gmail.com",
-  membership: "Platinum Account",
-  password: "demo",
-  membersince: "Jan 15, 2021",
+  membership: "Platinum Member",
+  membersince: "April 2021",
   profilePhoto: require("../assets/hiker.jpg"),
-  favorites: [
-    {
-      id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28b",
-      title: "Sunflower",
-      image: require("../assets/Sunflower.jpg"),
-    },
-    {
-      id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f6",
-      title: "Daisies",
-      image: require("../assets/Daisies.jpg"),
-    },
-    {
-      id: "58694a0f-3da1-471f-bd96-145571e29d7",
-      title: "Lavender",
-      image: require("../assets/Lavender.jpg"),
-    },
-    {
-      id: "bd7acbea-cb1-46c2-aed5-3ad53abb8",
-      title: "Sunflower",
-      image: require("../assets/Sunflower.jpg"),
-    },
-    {
-      id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f",
-      title: "Daisies",
-      image: require("../assets/Daisies.jpg"),
-    },
-    {
-      id: "58694a0f-3da1-471f-bd96-145571e29d",
-      title: "Lavender",
-      image: require("../assets/Lavender.jpg"),
-    },
-    {
-      id: "bd7abea-c1b1-46c2-aed5-3ad53abb28",
-      title: "Sunflower",
-      image: require("../assets/Sunflower.jpg"),
-    },
-    {
-      id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f",
-      title: "Daisies",
-      image: require("../assets/Daisies.jpg"),
-    },
-    {
-      id: "58694a0f-3da1-471f-bd96-145571e29d",
-      title: "Lavender",
-      image: require("../assets/Lavender.jpg"),
-    },
-    {
-      id: "bd7acbea-c1b1-46c2-ae5-3ad53abb2",
-      title: "Sunflower",
-      image: require("../assets/Sunflower.jpg"),
-    },
-    {
-      id: "3ac68afc-c605-48d3-a4f8-fbd91aa9f",
-      title: "Daisies",
-      image: require("../assets/Daisies.jpg"),
-    },
-    {
-      id: "58694a0f-3da1-471f-bd96-145571e9d",
-      title: "Lavender",
-      image: require("../assets/Lavender.jpg"),
-    },
-  ],
+  favorites: [],
 };
