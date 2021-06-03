ae4f60ee57aafdedc58cbf50362c2f66f5375b70
Tim Kang <tikang.cs@gmail.com>
2021-06-03 19:15:41 -0400

    #56 - Added the functional component code

--- app/screens/NewItemScreen.js
+++ app/screens/NewItemScreen.js
@@ -1,1 +1,10 @@
 import React from "react";
+import { View, Text } from "react-native";
+
+const newItemScreen = () => {
+  return (
+    <View>
+      <Text>New Items Screen</Text>
+    </View>
+  );
+};
