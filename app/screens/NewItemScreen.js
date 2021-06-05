6a6f10c3c11dbf0b69d1952ddf08599c4e7f3df9
Tim Kang <tikang.cs@gmail.com>
2021-06-04 23:19:13 -0400

    #57 - minor update to new item screen

--- app/screens/NewItemScreen.js
+++ app/screens/NewItemScreen.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-const newItemScreen = () => {
+export const newItemScreen = () => {
   return (
     <View>
       <Text>New Items Screen</Text>
